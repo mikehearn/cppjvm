ec0a4c15893f8e5c25374f731267e1f76b208370
Mike Hearn <mike@plan99.net>
2013-07-16 22:40:17 +0200

    Local hack to make it convert bitcoinj

--- java/cppwrap/cppwrap.sh
+++ java/cppwrap/cppwrap.sh
@@ -1,1 +1,1 @@
-java -cp java/cppwrap/release/prebuilt_jar/cppwrap.jar com.earwicker.cppjvm.cppwrap.CppWrap $@
+java -cp java/cppwrap/release/prebuilt_jar/cppwrap.jar:/Users/mike/Documents/Source/Bitcoinj/tools/target/bitcoinj-tools-0.10-SNAPSHOT.jar com.earwicker.cppjvm.cppwrap.CppWrap $@
